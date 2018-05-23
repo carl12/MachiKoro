856a2391b3107222a8f4536c18876d32e2ee7e9c
carl12 <carl.rodriguez13@gmail.com>
2018-05-22 17:34:10 -0700

    Moved player into seperate file

---
+++ Player.py
@@ -0,0 +1,90 @@
+import Cards
+
+class Player:
+    def __init__(self, game, strat=1, name = None):
+        self.game = game
+        self.money = 4
+        self.my_b = []
+        self.strat = strat
+
+        self.name = name if name else strat
+        self.landmarks = [Cards.Station(), Cards.ShoppingMall(), Cards.AmusementPark(), Cards.RadioTower()]
+
+
+    def has_won(self):
+        return not False in [a.owned for a in self.landmarks]
+
+    def get_reward(self, roll, is_turn):
+        bonuses = {"Shopping Mall": self.landmarks[1].owned}
+        for b in self.my_b:
+            if b:
+                reward = b.check(roll, is_turn, bonuses)
+                if reward:
+                    print('Player:',self.name,'just got',reward)
+                    self.money += reward
+        return self.money
+
+    def buy(self, num):
+        if num < 0:
+            num = -num + -1
+            if not self.landmarks[num].owned and self.money >= self.landmarks[num].cost:
+                pending = self.landmarks[num]
+                self.money -= pending.cost
+                return pending.buy()
+            return
+        pending = self.game.take(num)
+        if pending and pending.cost <= self.money:
+            self.money -= pending.cost
+            return pending
+
+    def print_remain(self):
+        for b in self.game.buildList:
+            print(b.name,str(b.remain),", ",end="")
+        print()
+        print(self.landmarks)
+
+    def buy_name(self, name):
+        return None
+
+    def cafe_steal(self):
+        pass
+
+    def choose_buy(self):
+        if self.strat == 0:
+            self.print_remain()
+            print("You have $"+str(self.money))
+            print("Your buildings:",self.my_b)
+            choice = input("choose a building")
+
+            a = int(choice)
+            building = self.buy(a)
+            if building:
+                print("Human built ", building.name)
+                self.my_b.append(building)
+            else:
+                print("Nothing bought")
+
+        if self.strat == 1:
+            if self.money >= 1:
+                build = self.buy(1)
+                if build:
+                    self.my_b.append(build)
+                    print('Strat 1 built a ranch')
+        if self.strat == 2:
+            if self.money >= 2:
+                build = self.buy(4)
+                if build:
+                    self.my_b.append(build)
+                    print('Strat 2 built a Convenience Store')
+
+    def tv_steal(self):
+        pass
+
+    def business_center(self):
+        pass
+
+    def roll2(self):
+        return False
+
+    def reroll(self, total, doubles = False):
+        return True
